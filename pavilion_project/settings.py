0f8ec6e8f820130eeefacc82262919e532e0e409
Shayan Ahmed <ahmedshayan443@gmail.com>
2026-07-07 11:30:13 +0500

    Initial commit

--- pavilion_project/settings.py
+++ pavilion_project/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'sports_pavilion',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'tiger',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
